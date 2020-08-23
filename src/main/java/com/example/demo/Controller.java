aaf1325c52dbdc924cc1cd5c758674b5024dc192
Chanyut Yuvacharuskul <chanyut@wongnai.com>
2020-08-23 14:49:38 +0700

    allow cors

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -16,10 +13,7 @@ import java.nio.file.Files;
 @RequestMapping("/")
 public class Controller {
 
-    //Todo: eliminate all warning (folder -> analyze -> inspect code)
-    // React
-    // Heroku
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/")
     public String interact(@RequestBody String input) throws IOException {
 
